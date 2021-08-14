0244c497317168dfdc7675730927ebd2b9263032
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-08-13 21:19:49 -0700

    re-org index in attempt to make more responsive.

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,11 @@
+let burgerOpen = false;
+function openMenu() {
+    const headerMenuStyle = document.getElementById("headerNav").style;
+    headerMenuStyle.backgroundColor = '#ffaaff';
+    headerMenuStyle.display = 'none';
+    headerMenuStyle.position = 'top: 500px;';
+
+
+
+
+}
